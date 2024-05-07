07e63147feb5396e9bef6f50d292e98ab87b52fa
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-07 14:25:57 +0100

    3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -40,11 +40,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-    ) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return hypermedia pagination details for
         the given index and page size.
 
