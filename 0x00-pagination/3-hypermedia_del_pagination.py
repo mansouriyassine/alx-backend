91fad56c8b6fcc1197ed983f287b847c8be454d2
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-07 14:14:30 +0100

    0

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,7 +4,9 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-from typing import List, Dict
+from typing import List
+from typing import Dict
+from typing import Union
 
 
 class Server:
@@ -37,7 +39,7 @@ class Server:
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
-
+    
     def get_hyper_index(
         self,
         index: int = None,
@@ -47,10 +49,8 @@ class Server:
         the given index and page size.
 
         Args:
-            index (int, optional): The start index of the current page.
-            Defaults to None.
-            page_size (int, optional): The number of items per page.
-            Defaults to 10.
+            index (int, optional): The start index of the current page. Defaults to None.
+            page_size (int, optional): The number of items per page. Defaults to 10.
 
         Returns:
             dict: A dictionary containing pagination details.
