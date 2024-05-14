d8fa285c223ecd0a69cc28acbd2f9dbda5cd9560
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-14 14:14:34 +0100

    1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,29 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+1-app Module
+
+Basic Flask app with Babel setup.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """Configuration class for Flask app."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+@app.route('/')
+def index():
+    """Route to render index.html."""
+    return render_template('1-index.html', title='Welcome to Holberton',
+                           header='Hello world')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
