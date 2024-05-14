346066efa9548d03e62cbfb889ea14e91236ef17
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-14 14:58:57 +0100

    4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ app.config.from_object(Config)
 def get_locale():
     """Determine the best match for the supported languages."""
     if 'locale' in request.args and \
-        request.args['locale'] in app.config['LANGUAGES']:
+            request.args['locale'] in app.config['LANGUAGES']:
         return request.args['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
